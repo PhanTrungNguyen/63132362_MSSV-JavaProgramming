ff6614802ee1aaaeab2cbaf3e5bdd0b1b192adc6
PhanTrungNguyen <145771479+PhanTrungNguyen@users.noreply.github.com>
2024-01-18 12:59:53 +0700

    Ví dụ đầu tiên
    chỉ để demo

---
+++ HelloWorld/src/tiil/edu/XinChao.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package tiil.edu;
+
+/**
+ * 
+ */
+public class XinChao {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//
+		System.out.println("Lan dau hoc Java");
+
+	}
+
+}
